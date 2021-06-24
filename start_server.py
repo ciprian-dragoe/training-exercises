4788ef5d46f8d4bd49144b30e6b8f192a8e1a461
Ciprian Dragoe <cip.love.life@gmail.com>
2021-06-24 16:28:28 +0300

    modified logic for generating new logic

--- start_server.py
+++ start_server.py
@@ -3,19 +3,11 @@ app = Flask('dom-manipulation')
 from os import path, listdir
 
 
-# @app.route('/<template_number>')
-# def index(template_number):
-#     try:
-#         return render_template(f"index{template_number}.html", template_number=template_number)
-#     except:
-#         return f"The template number you were looking for is not found: <h1>{template_number}</h1>"
-
-
 @app.route('/exercises/<exercise_number>/<file_name>')
 def getExercise(exercise_number, file_name):
     try:
         create_user_files(file_name, exercise_number)
-        return render_template(f"{file_name}.html", file_name=file_name)
+        return render_template(f"{file_name}-{exercise_number}.html", file_name=file_name, exercise_number=exercise_number)
     except:
         return f"Could not create template with name : <h1>{file_name}</h1>"
 
@@ -23,16 +15,16 @@ def getExercise(exercise_number, file_name):
 def create_user_files(file_name, exercise_number):
     exercise_file_names = get_exercise_files(exercise_number)
     for template_name in exercise_file_names:
-        create_from_template_if_not_exist(template_name, file_name)
+        create_from_template_if_not_exist(template_name, file_name, exercise_number)
 
 
 def get_exercise_files(exercise_number):
     files = [f'./exercises/{exercise_number}/{file}' for file in listdir(f'./exercises/{exercise_number}/')]
-    return files
+    return files  
 
 
-def create_from_template_if_not_exist(template, file_name):
-    file_path = get_file_path(template, file_name)
+def create_from_template_if_not_exist(template, file_name, exercise_number):
+    file_path = get_file_path(template, file_name, exercise_number)
     if not path.exists(file_path):
         with open(template, "r") as file:
             content = file.read()
@@ -40,23 +32,18 @@ def create_from_template_if_not_exist(template, file_name):
             file.write(content)
 
 
-def get_file_path(template, file_name):
+def get_file_path(template, file_name, exercise_number):
     extension = template.split(".")[-1]
     if extension == "html":
-        return f"templates/{file_name}.html"
+        return f"templates/{file_name}-{exercise_number}.html"
     if extension == "js":
-        return f"static/js/{file_name}.js"
+        return f"static/js/{file_name}-{exercise_number}.js"
     if extension == "css":
-        return f"static/style/{file_name}.css"
-
-
-def get_file_content(template):
-    with open(template, "r") as file:
-        return file.read()
+        return f"static/style/{file_name}-{exercise_number}.css"
 
 
 def main():
-    app.run(debug=True)
+    app.run()
 
 
 if __name__ == '__main__':
