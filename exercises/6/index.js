b5e1703eddbff1eccedc746d9ddeb64898e8781c
Ciprian Dragoe <cip.love.life@gmail.com>
2021-06-25 12:24:53 +0300

    added extra exercises

---
+++ exercises/6/index.js
@@ -0,0 +1,4 @@
+console.log(6)
+
+// when you click on the button change the button's name to you `you clicked me !`
+
---
+++ exercises/7/index.js
@@ -0,0 +1,3 @@
+console.log(7)
+
+// whenever you type something in the input box, say if it is an even or odd number (or not a number at all)
