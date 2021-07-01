2dd849372be4dd9ecf239ac52a5d9a9fe8aa1e0d
Ciprian Dragoe <cip.love.life@gmail.com>
2021-07-01 15:20:15 +0300

    table is displayed

--- exercises/11/index.js
+++ exercises/11/index.js
@@ -23,7 +23,42 @@ async function executeQuery() {
         body: JSON.stringify({ query: textArea.value})
     })
     const data = await response.json()
-    console.log(data)
+    buildTable("query-result", data)
+}
+
+function generateHtmlTableHeaders(headers) {
+    let result = ""
+    headers.forEach(header => {
+        result = result + `<td>${header}</td>`
+    })
+    return "<tr>" + result + "</tr>"
+}
+
+function generateHtmlTableBody(items) {
+    let result = ""
+    items.forEach(item => {
+        let row = ""
+        Object.values(item).forEach(cell => {
+            row = row + `<td>${cell}</td>`
+        })
+        result = result + `<tr>${row}</tr>`
+    })
+    return result
+}
+
+function buildTable(placementId, tableData) {
+    const tableHeaders = Object.keys(tableData[0])
+    const table = document.getElementById(placementId)
+    table.innerHTML += `
+<table class="table table-striped table-hover">
+    <thead>
+        ${generateHtmlTableHeaders(tableHeaders)}
+    </thead>
+    <tbody>
+        ${generateHtmlTableBody(tableData)}
+    </tbody>
+</table>
+    `
 }
 
 document.getElementById("run-query-button").addEventListener("click", executeQuery)
