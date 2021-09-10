a31075f20b72bf5f71dc5d5f31847db79ec95c9f
Ciprian Dragoe <cip.love.life@gmail.com>
2021-09-10 19:11:39 +0300

    added js basic code formating

---
+++ static/js/code-formatter.js
@@ -0,0 +1,65 @@
+const codeElement = document.getElementById("code-area")
+const runElement = document.getElementById("run-code")
+const tabSize = 2
+codeElement.focus()
+
+codeElement.addEventListener("keyup", event => {
+    if (event.key === "Enter") {
+        sendEnter()
+    }
+})
+
+codeElement.addEventListener("keydown", event => {
+    console.log(event)
+    if (event.key === "Tab") {
+        sendTab()
+    }
+})
+
+function sendTab() {
+    const currentCursorPosition = codeElement.selectionStart
+    codeElement.value = [
+        codeElement.value.slice(0, codeElement.selectionStart),
+        " ".repeat(tabSize),
+        codeElement.value.slice(codeElement.selectionStart)]
+        .join('')
+    codeElement.selectionStart = currentCursorPosition + 2
+    codeElement.selectionEnd = currentCursorPosition + 2
+    setTimeout(() => codeElement.focus(), 0)
+}
+
+function sendEnter() {
+    const currentCursorPosition = codeElement.selectionStart
+    const cursorPosition = getCursorPosition(codeElement)
+    const currentLine = codeElement.value.split("\n")[cursorPosition.lineNumber]
+    const spacesPreviousLine = currentLine.length - currentLine.trimLeft().length
+    const spacesFromNewBlockScope = currentLine.trimRight().at(-1) === "{" ? 2 : 0
+    const newLineIndentation = " ".repeat(spacesPreviousLine + spacesFromNewBlockScope)
+    codeElement.value = [
+        codeElement.value.slice(0, currentCursorPosition),
+        newLineIndentation,
+        codeElement.value.slice(currentCursorPosition)]
+        .join('')
+    codeElement.selectionStart = currentCursorPosition + spacesPreviousLine + spacesFromNewBlockScope
+    codeElement.selectionEnd = currentCursorPosition + spacesPreviousLine + spacesFromNewBlockScope
+}
+
+function getCursorPosition(inputElement) {
+    const currentPosition = inputElement.selectionStart - 1
+    const lines = codeElement.value.split("\n")
+    const result = {
+        lineNumber: 0,
+        colNumber: 0
+    }
+    let totalCharactersTraversed = 0
+    for (let i=0; i<lines.length; i++) {
+        result.lineNumber = i
+        if (totalCharactersTraversed + lines[i].length + 1 > currentPosition) {
+            result.colNumber = currentPosition - totalCharactersTraversed
+            break
+        }
+        totalCharactersTraversed += lines[i].length + 1
+    }
+
+    return result
+}
