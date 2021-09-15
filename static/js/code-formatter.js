51d76ee8544996f536a8065f9513f40ff0f9ad81
Ciprian Dragoe <cip.love.life@gmail.com>
2021-09-15 13:41:34 +0300

    refactored key send

--- static/js/code-formatter.js
+++ static/js/code-formatter.js
@@ -3,16 +3,10 @@ const runElement = document.getElementById("run-code")
 const tabSize = 2
 codeElement.focus()
 
-codeElement.addEventListener("keyup", event => {
-    if (event.key === "Enter") {
-        sendEnter()
-    }
-})
-
 codeElement.addEventListener("keydown", event => {
-    console.log(event)
-    if (event.key === "Tab") {
-        sendTab()
+    const action = keyAction[event.key]
+    if (action) {
+        action()
     }
 })
 
@@ -29,19 +23,21 @@ function sendTab() {
 }
 
 function sendEnter() {
-    const currentCursorPosition = codeElement.selectionStart
-    const cursorPosition = getCursorPosition(codeElement)
-    const currentLine = codeElement.value.split("\n")[cursorPosition.lineNumber]
-    const spacesPreviousLine = currentLine.length - currentLine.trimLeft().length
-    const spacesFromNewBlockScope = currentLine.trimRight().at(-1) === "{" ? 2 : 0
-    const newLineIndentation = " ".repeat(spacesPreviousLine + spacesFromNewBlockScope)
-    codeElement.value = [
-        codeElement.value.slice(0, currentCursorPosition),
-        newLineIndentation,
-        codeElement.value.slice(currentCursorPosition)]
-        .join('')
-    codeElement.selectionStart = currentCursorPosition + spacesPreviousLine + spacesFromNewBlockScope
-    codeElement.selectionEnd = currentCursorPosition + spacesPreviousLine + spacesFromNewBlockScope
+    setTimeout(() => {
+        const currentCursorPosition = codeElement.selectionStart
+        const cursorPosition = getCursorPosition(codeElement)
+        const currentLine = codeElement.value.split("\n")[cursorPosition.lineNumber]
+        const spacesPreviousLine = currentLine.length - currentLine.trimLeft().length
+        const spacesFromNewBlockScope = currentLine.trimRight().at(-1) === "{" ? 2 : 0
+        const newLineIndentation = " ".repeat(spacesPreviousLine + spacesFromNewBlockScope)
+        codeElement.value = [
+            codeElement.value.slice(0, currentCursorPosition),
+            newLineIndentation,
+            codeElement.value.slice(currentCursorPosition)]
+            .join('')
+        codeElement.selectionStart = currentCursorPosition + spacesPreviousLine + spacesFromNewBlockScope
+        codeElement.selectionEnd = currentCursorPosition + spacesPreviousLine + spacesFromNewBlockScope
+    }, 0)
 }
 
 function getCursorPosition(inputElement) {
@@ -62,4 +58,9 @@ function getCursorPosition(inputElement) {
     }
 
     return result
+}
+
+const keyAction = {
+    "Tab": sendTab,
+    "Enter": sendEnter
 }
