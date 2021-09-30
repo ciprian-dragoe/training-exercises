5750ee7706ee5e49c1b344febf349b63c750c441
Ciprian Dragoe <cip.love.life@gmail.com>
2021-09-30 17:53:06 +0300

    added separation per student & exercise

--- static/js/admin-dashboard.js
+++ static/js/admin-dashboard.js
@@ -5,15 +5,26 @@ async function getActiveProjects() {
 }
 
 function displayActiveExercises(exercises) {
-    const destinationElement = document.getElementById("active-exercises")
     let result = ""
-    for (let exercise of exercises) {
+    const destinationElement = document.getElementById("active-exercises")
+    const users = [...new Set(exercises.map(e => e.user))]
+    for (let user of users) {
+        const exerciseNumbers = exercises.filter(e => e.user === user).map(e => e.exerciseNumber)
+        const links = exerciseNumbers.map(exNum => `
+            <div>
+                ${exNum}
+                <button target="_blank" href="/exercises/${exNum}/${user}">Go to exercise</button>
+                <button id="${user}-${exNum}">View solution</button>
+            </div>`
+        ).join("")
         result += `
-        <div><a target="_blank" href="/exercises/${exercise.exerciseNumber}/${exercise.user}">
-            ${exercise.user} - ${exercise.exerciseNumber.replaceAll("_", " ")}
-        </a></div>`
+            <div>
+                <div class="user">${user}</div>
+                <div class="links">${links}</div>
+            </div>`
     }
     destinationElement.innerHTML = result
 }
 
+getActiveProjects()
 setInterval(getActiveProjects, 10000)
