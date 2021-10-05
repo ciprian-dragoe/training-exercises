d8b646af757d3aab29df199d15e8227d1285f50f
Ciprian Dragoe <cip.love.life@gmail.com>
2021-10-05 19:56:08 +0300

    fixed admin bug when user enters name with space in it

--- static/js/exercises.js
+++ static/js/exercises.js
@@ -1,12 +1,11 @@
 document.querySelector("form").addEventListener("submit", (e) => {
     e.preventDefault();
-    const name = e.target.name.value.toLowerCase().replace(/\s+/gim, "-");
+    const name = e.target.name.value.toLowerCase().replace(/\s+/gim, "=");
     const number = e.target.number.value;
 
     if (!name) {
         alert("Name must not be empty!");
         return;
     }
-
-    window.location.pathname = `/exercises/${number}/${name.replaceAll(" ", "-")}`;
+    window.location.pathname = `/exercises/${number}/${name}`;
 });
