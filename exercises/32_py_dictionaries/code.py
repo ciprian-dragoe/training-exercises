291495906b83a452f329b7fa4ea860975a4067aa
Ciprian Dragoe <cip.love.life@gmail.com>
2022-02-18 14:43:13 +0200

    added python dictionaries

---
+++ exercises/32_py_dictionaries/code.py
@@ -0,0 +1,31 @@
+a = {
+  "nume": {
+    "familie": "dragoe",
+    "mijlociu": "maniu",
+    "mic": "cipi",
+    "nickname": {
+      "copilarie": "speedy",
+      "adolescenta": "gigel"
+    }
+  },
+  "varsta": {
+    "luna": 7,
+    "ziua": 14,
+    "an": 1987
+  },
+  "hobby": ["fotbal", "citit", "pescuit"],
+  "ticTacToe": [
+    [1, 2, 3],
+    [4, 5, 6],
+    [7, 8, 9]
+  ],
+  "loc_munca": "codecool",
+  "in_a_relationship": False
+}
+# scrieti printul sa apara 'codecool' accesand cheile din dictionarul in variabila 'a'
+# scrieti printul sa apara 'dragoe' accesand cheile din dictionarul in variabila 'a'
+# scrieti printul sa apara '9' accesand cheile din dictionarul in variabila 'a'
+
+# incrementati cu 1 luna in care s-a nascut
+# adaugati un nou nickname, pentru perioada 'adulta'
+# adaugati un hobby nou
