ebfbf8168a803cb18e664cef65c0701bebb5e367
Ciprian Dragoe <cip.love.life@gmail.com>
2022-03-24 19:55:37 +0200

    work on database configuration

--- app/services/database.py
+++ app/services/database.py
@@ -37,13 +37,13 @@ def get_connection_data(db_name=None):
     :db_name: optional parameter. By default it uses the environment variable value.
     """
     if db_name is None:
-        db_name = os.environ.get("MY_PSQL_DBNAME")
+        db_name = os.environ.get("PRACTICE_DBNAME")
 
     return {
         "dbname": db_name,
-        "user": os.environ.get("MY_PSQL_USER"),
-        "host": os.environ.get("MY_PSQL_HOST"),
-        "password": os.environ.get("MY_PSQL_PASSWORD"),
+        "user": os.environ.get("PRACTICE_USER"),
+        "host": os.environ.get("PRACTICE_HOST"),
+        "password": os.environ.get("PRACTICE_PASS"),
     }
 
 
--- app/services/docker.py
+++ app/services/docker.py
@@ -2,12 +2,12 @@ import subprocess
 from data.configuration import CONFIGURATION
 
 
-def create_python_container(image_name):
-    return str(subprocess.check_output(f"docker run -td {image_name} tail", shell=True))[2:-3]
+def launch_container(image_name, extra_arguments=""):
+    return str(subprocess.check_output(f"docker run -td {extra_arguments} {image_name} tail", shell=True))[2:-3]
 
 
 def get_python_execution_result(file, language):
-    if not CONFIGURATION["DOCKER_CONTAINER_ID"]:
+    if not CONFIGURATION["LANGUAGE_PY_CONTAINER_ID"]:
         raise Exception("Admin is not logged in => code execution is disabled")
     handler = DOCKER_EXECUTION_TEMPLATES[language]
     output = handler(file)
@@ -17,8 +17,8 @@ def get_python_execution_result(file, language):
 
 def execute_python(file_path):
     file_name = file_path.split("/")[-1]
-    subprocess.check_output(f"docker cp {file_path} {CONFIGURATION['DOCKER_CONTAINER_ID']}:/{file_name}", shell=True)
-    output = str(subprocess.check_output(f'docker exec {CONFIGURATION["DOCKER_CONTAINER_ID"]} python /{file_name}', shell=True, stderr=subprocess.STDOUT))[2:-3]
+    subprocess.check_output(f"docker cp {file_path} {CONFIGURATION['LANGUAGE_PY_CONTAINER_ID']}:/{file_name}", shell=True)
+    output = str(subprocess.check_output(f'docker exec {CONFIGURATION["LANGUAGE_PY_CONTAINER_ID"]} python /{file_name}', shell=True, stderr=subprocess.STDOUT))[2:-3]
     return output
 
 
@@ -28,11 +28,16 @@ DOCKER_EXECUTION_TEMPLATES = {
 
 
 def kill_existing_containers():
-    if CONFIGURATION["DOCKER_CONTAINER_ID"]:
-        subprocess.check_output(f'docker kill {CONFIGURATION["DOCKER_CONTAINER_ID"]}', shell=True)
-    CONFIGURATION["DOCKER_CONTAINER_ID"] = None
+    if CONFIGURATION["LANGUAGE_PY_CONTAINER_ID"]:
+        subprocess.check_output(f'docker kill {CONFIGURATION["LANGUAGE_PY_CONTAINER_ID"]}', shell=True)
+    if CONFIGURATION["LANGUAGE_PG_CONTAINER_ID"]:
+        subprocess.check_output(f'docker kill {CONFIGURATION["LANGUAGE_PG_CONTAINER_ID"]}', shell=True)
+    CONFIGURATION["LANGUAGE_PY_CONTAINER_ID"] = None
+    CONFIGURATION["LANGUAGE_PG_CONTAINER_ID"] = None
 
 
 def initialize():
-    if not CONFIGURATION["DOCKER_CONTAINER_ID"]:
-        CONFIGURATION["DOCKER_CONTAINER_ID"] = create_python_container("training-exercises_language-py")
+    if not CONFIGURATION["LANGUAGE_PY_CONTAINER_ID"]:
+        CONFIGURATION["LANGUAGE_PY_CONTAINER_ID"] = launch_container("training-exercises_language-py")
+    if not CONFIGURATION["LANGUAGE_PG_CONTAINER_ID"]:
+        CONFIGURATION["LANGUAGE_PG_CONTAINER_ID"] = launch_container("training-exercises_language-pg", f"-p {CONFIGURATION['PRACTICE_PORT']}:5432")
