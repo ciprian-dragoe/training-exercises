66240d4b1a10cc63891f71b5e465c78b00db9801
Ciprian Dragoe <cip.love.life@gmail.com>
2022-04-04 17:40:21 +0300

    db sync compiles but has bugs

--- app/data/entities/starter_files.py
+++ app/data/entities/starter_files.py
@@ -8,3 +8,32 @@ def get_all():
         FROM starter_files f
         inner join starter_projects p on f.starter_project_id = p.id
     """)
+
+
+def create(file):
+    connection = database.get_db_connection(database.DbConnection.admin)
+    return database.execute(connection, f"""
+            insert into starter_files(name, content, starter_project_id)
+            values(%(file_name)s, %(file_content)s, {file['starter_project_id']}) 
+            returning *
+        """, {
+        'file_name': file['name'],
+        'file_content': file['content'],
+    })[0]
+
+
+def delete(file_id):
+    connection = database.get_db_connection(database.DbConnection.admin)
+    return database.execute(connection, f"""
+            delete from starter_files where id = {file_id}
+        """)
+
+
+def update_content(file_id, new_content):
+    connection = database.get_db_connection(database.DbConnection.admin)
+    return database.execute(connection, f"""
+                update starter_files
+                set content = %(new_content)s) 
+                where id = {file_id}
+                returning *;
+            """, { 'new_content': new_content})[0]
