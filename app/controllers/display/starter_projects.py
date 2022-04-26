3a16dd73302eff4837769dab705fba99283e2b9b
Ciprian Dragoe <cip.love.life@gmail.com>
2022-04-26 17:54:33 +0300

    temporary fix for upgrading https requests

--- app/controllers/display/starter_projects.py
+++ app/controllers/display/starter_projects.py
@@ -1,4 +1,4 @@
-from flask import Blueprint, render_template
+from flask import Blueprint, render_template, request
 
 
 display_starter_projects = Blueprint('starter-projects', __name__)
@@ -6,4 +6,5 @@ display_starter_projects = Blueprint('starter-projects', __name__)
 
 @display_starter_projects.route("/")
 def render_exercises():
-    return render_template("starter-projects.html")
+    upgrade_to_https = request.host_url[-8:-1] == 'loca.lt'
+    return render_template("starter-projects.html", upgrade_to_https=upgrade_to_https)
